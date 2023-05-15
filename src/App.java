626d541c96843dab2061e86590ff13c936cf16d4
Moniroth-03 <127386659+Moniroth-03@users.noreply.github.com>
2023-05-15 19:39:15 +0700

    i alr fix my size

--- src/App.java
+++ src/App.java
@@ -11,13 +11,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("List_Book.fxml"));
-=======
-        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
->>>>>>> 3fc1a76a1734ec64f8b2ef527bd78352ce166227
         stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root, 800, 800));
+        stage.setScene(new Scene(root));
         stage.show();
     }
 }
