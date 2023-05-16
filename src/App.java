4e98760514c44eb622de2003e30edb6fc82c0655
sreylenchab <126034400+CHAB-Sreylen@users.noreply.github.com>
2023-05-16 17:10:29 +0700

    add

--- src/App.java
+++ src/App.java
@@ -3,27 +3,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+//import javafx.stage.StageStyle;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
-
     @Override
     public void start(Stage stage) throws Exception {
-<<<<<<< HEAD
         Parent root = FXMLLoader.load(getClass().getResource("loginpage.fxml"));
-        stage.initStyle(StageStyle.UNDECORATED);
+        //stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root, 1200, 800));
-=======
-
-        Parent root = FXMLLoader.load(getClass().getResource("my_categories.fxml"));
-
-        stage.setTitle("Welcome to Library App");
-        stage.setScene(new Scene(root));
->>>>>>> 1ebe5bf3387854453b0d62fb08a5aec04e2ef1e3
+        stage.setScene(new Scene(root, 900, 600));
         stage.show();
+
+
+        
     }
 }
