29ed6d5db27006766c50629df1a4dc110d75c40b
sreylenchab <126034400+CHAB-Sreylen@users.noreply.github.com>
2023-05-16 18:19:40 +0700

    add

--- src/App.java
+++ src/App.java
@@ -5,19 +5,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         launch(args);
     }
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("loginpage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("my_categories.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
         //stage.setTitle("Welcome to Library App");
         stage.setScene(new Scene(root, 900, 600));
         stage.show();
-
-
-        
     }
+  
 }
--- src/controller.java
+++ src/controller.java
@@ -1,16 +1,22 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class controller {
     @FXML
     private Button cancelButton;
+    @FXML
+    private Label loginButtonlLabel;
+
+    public void loginButtonlLabelOnAction(ActionEvent e){
+        loginButtonlLabel.setText("You did not have account");
+    }
     public void cancelButtonOnAction(ActionEvent e){
          Stage stage = (Stage) cancelButton.getScene().getWindow();
          stage.close();
- 
-    }
 
-   
+    }
+  
 }
