7265a4fc9b73702a0de71aaaa68ee5a8c8d13dcf
HENG-Menghy <hengmenghy2002@gmail.com>
2023-05-25 16:18:05 +0700

    add new file Update_Book.fxml

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+//import javafx.stage.StageStyle;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
@@ -12,8 +12,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("add_book.fxml"));
-        stage.initStyle(StageStyle.UNDECORATED);
+        Parent root = FXMLLoader.load(getClass().getResource("Update_Book.fxml"));
+        //stage.initStyle(StageStyle.UNDECORATED);
         // stage.setTitle("Welcome to Library App");
         stage.setScene(new Scene(root));
         stage.show();
