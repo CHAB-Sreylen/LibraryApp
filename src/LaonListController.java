157e18fe3cda813797602ccd79a6930031e0b19e
sreylenchab <126034400+CHAB-Sreylen@users.noreply.github.com>
2023-06-07 14:01:29 +0700

    updateLaonList

--- src/LaonListController.java
+++ src/LaonListController.java
@@ -1,46 +1,105 @@
+
+import java.beans.BeanProperty;
 import java.io.IOException;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import java.util.ResourceBundle;
 
-public class LaonListController {
-    private Stage ReturnButton;
-    private Stage LaonFormButton;
-    private Stage ReturnFormButton;
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class LaonListController implements Initializable {
+    public void initialize(URL url, ResourceBundle resourceBundle) {// initialize the table value
+
+    }
+    private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchtoReturnList(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Return_list.fxml"));
-        ReturnButton = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        ReturnButton.setScene(scene);
-        ReturnButton.show();
-    }
-    
-    public void switchtoLaonForm(ActionEvent event ) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("LaonForm.fxml"));
-        LaonFormButton = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        LaonFormButton.setScene(scene);
-        LaonFormButton.show();
+    @FXML
+    private Button Delete2;
+
+    @FXML
+    private Button LaonForm;
+
+    @FXML
+    private Button ReturnForm;
+
+    @FXML
+    private Button ReturnList;
+
+    @FXML
+    private Button back1;
+
+    Connection con;
+    PreparedStatement pst;
+    ResultSet rs;
+
+    public void back1OnAction(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(getClass().getResource("ListBook.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
-    public void switchtoReturnForm(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("ReturnForm.fxml"));
-        ReturnFormButton = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        ReturnFormButton.setScene(scene);
-        ReturnFormButton.show();
+    // public void delete2OnAction(ActionEvent event) throws IOException {
+    //     Object root = FXMLLoader.load(getClass().getResource("ListBook.fxml"));
+    //     Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    //     Scene scene = new Scene((Parent) root);
+    //     stage.setScene(scene);
+    //     stage.show();
+
+    // }
+    
+    public void LaonFormOnAction(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(getClass().getResource("LaonForm.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+
     }
+
+    public void ReturnFormOnAction(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(getClass().getResource("ReturnForm.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void ReturnListOnAction(ActionEvent event) throws IOException {
+        Object root = FXMLLoader.load(getClass().getResource("Return_list.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene((Parent) root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+
 }
+
+
+   
+
