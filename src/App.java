8a95fc688b783bda3b2fb4b44b08a6b028a20742
HENG-Menghy <hengmenghy2002@gmail.com>
2023-06-13 16:48:44 +0700

    modify

--- src/App.java
+++ src/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
 
         // stage.initStyle(StageStyle.UNDECORATED);
 
-        Parent root = FXMLLoader.load(getClass().getResource("loginpage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UpdateBook.fxml"));
 
         // stage.initStyle(StageStyle.UNDECORATED);
         // stage.setTitle("Welcome to Library App");
